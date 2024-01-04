0a66c54c1255235ea153fc0b4c29b1e830239ced
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2024-01-04 21:08:50 +0900

    first-commit

---
+++ Regional-Compatibility-Checker/Regional-Compatibility-Checker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Regional-Compatibility-Checker
+//
+//  Created by k21123kk on 2024/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
