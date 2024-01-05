c4a0f0245df5795050bebcb8b4431ac0dd5e5040
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2024-01-05 22:32:47 +0900

    ユーザー情報を保持するモデルの作成

--- Regional-Compatibility-Checker/Regional-Compatibility-Checker/Models/User.swift
+++ Regional-Compatibility-Checker/Regional-Compatibility-Checker/Models/User.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+// 送信時の年月日を表す構造体
+struct YearMonthDay: Codable {
+    var year: Int
+    var month: Int
+    var day: Int
+}
+
+// ユーザー情報を表す構造体
+struct User: Codable {
+  var name: String
+  var birthday: YearMonthDay
+  var bloodType: String
+  var today: YearMonthDay
+}
