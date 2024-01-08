5664bbda48419538c210ef028b19814532dd7e71
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2024-01-08 20:44:28 +0900

    APIリクエストの実行やバリデーションチェック、ロゴのダウンロード処理などの実装を行った。

--- Regional-Compatibility-Checker/Regional-Compatibility-Checker/ViewModel/UserPrefectureViewModel.swift
+++ Regional-Compatibility-Checker/Regional-Compatibility-Checker/ViewModel/UserPrefectureViewModel.swift
@@ -6,12 +6,26 @@
 //
 
 import Foundation
+import UIKit
 
 class UserPrefectureViewModel: ObservableObject {
-  @Published var userData: UserData
-  @Published var prefectureData: PrefectureData?
-  @Published var isLoading: Bool = false
-  @Published var errorMessage: String?
+  @Published var userData: UserData // ユーザー情報
+  @Published var prefectureData: PrefectureData? { // APIのレスポンスデータ
+    didSet { // prefectureDataが更新されたら、ロゴ（イラスト）をダウンロードする
+      if let logoUrl = prefectureData?.logoUrl {
+        downloadPrefectureImage(from: logoUrl)
+      }
+    }
+  }
+  
+  @Published var isLoading: Bool = false // APIリクエスト中かどうか
+  @Published var errorMessage: String? // エラーメッセージ
+  @Published var nameErrorMessage: String? // 名前のエラーメッセージ
+  @Published var birthdayErrorMessage: String? // 誕生日のエラーメッセージ
+  @Published var bloodTypeErrorMessage: String? // 血液型のエラーメッセージ
+  
+  @Published var downloadedImage: UIImage? // 県のロゴ（イラスト）
+
   
   private var apiService: APIService
   
@@ -23,10 +37,6 @@ class UserPrefectureViewModel: ObservableObject {
   // MARK: - fetchPrefectureData()
   // APIリクエストを実装し、結果をハンドリングするメソッド
   func fetchPrefectureData() {
-    guard validateUserData(userData: self.userData) else {
-      self.errorMessage = "入力内容を確認してください。"
-      return
-    }
     self.isLoading = true
     self.errorMessage = nil
     
@@ -36,10 +46,12 @@ class UserPrefectureViewModel: ObservableObject {
     let year = calendar.component(.year, from: now)
     let month = calendar.component(.month, from: now)
     let day = calendar.component(.day, from: now)
+
+    self.userData.today = YearMonthDay(year: year, month: month, day: day)  // ユーザー情報(today)に現在時刻をセット
     
-    self.userData.today = YearMonthDay(year: year, month: month, day: day)
     apiService.postUserData(userData: self.userData) { [weak self] result in  // [weak self]を使って循環参照を防ぐ
-      DispatchQueue.main.async {
+      
+      DispatchQueue.main.async {  //メインスレッドで実行
         self?.isLoading = false
         switch result {
         case .success(let data):
@@ -51,34 +63,95 @@ class UserPrefectureViewModel: ObservableObject {
     }
   }
   
-  // ユーザー情報のバリデーションチェック
-  // MARK: - validateUserData()
-  func validateUserData(userData: UserData) -> Bool {
+
+  
+  // ユーザー名のバリデーションを行うメソッド
+  // MARK: - validateName()
+  func validateName() {
     if userData.name.isEmpty {
-      self.errorMessage = "名前を入力してください"
-      return false
+      nameErrorMessage = "名前を入力してください。"
+    } else {
+      nameErrorMessage = nil
     }
     
+    return
+  }
+  
+  // 誕生日のバリデーションを行うメソッド
+  // MARK: - validateBirthday()
+  func validateBirthday() {
+    self.birthdayErrorMessage = nil
+    
+    let calendar = Calendar(identifier: .gregorian)
+    let currentDate = Date()
+    var dateComponents = DateComponents()
+    dateComponents.year = userData.birthday.year
+    dateComponents.month = userData.birthday.month
+    dateComponents.day = userData.birthday.day
+    // 誕生日が未入力の場合のチェック
     if userData.birthday.year == 0 || userData.birthday.month == 0 || userData.birthday.day == 0 {
-      self.errorMessage = "生年月日を入力してください"
-      return false
+      self.birthdayErrorMessage = "誕生日を入力してください。"
+      return
     }
-    
+
+    // 日付の形式が正しいかどうかのチェック
+    guard let birthdayDate = calendar.date(from: dateComponents) else {
+      self.birthdayErrorMessage = "無効な日付です。"
+      return
+    }
+
+    // 誕生日が未来の日付でないかのチェック
+    if birthdayDate > currentDate {
+      self.birthdayErrorMessage = "誕生日が未来の日付です。"
+      return
+    }
+  }
+  
+  // 血液型のバリデーションを行うメソッド
+  // MARK: - validateBloodType()
+  func validateBloodType() {
     if userData.bloodType.isEmpty {
-      self.errorMessage = "血液型を入力してください"
-      return false
+      bloodTypeErrorMessage = "血液型を入力してください。"
+    } else if !["a", "b", "o", "ab"].contains(userData.bloodType) {  // 入力された血液型がA, B, O, ABのいずれかでない場合
+      bloodTypeErrorMessage = "A, B, O, ABのいずれかを入力してください。"
+    }
+    else {
+      bloodTypeErrorMessage = nil
     }
     
-    return true
+    return
   }
   
-  // prefectureDataをビューから使いやすいように必要なデータ形式に変換
-  // MARK: - getPrefectureData()
-  func getPrefectureData() -> PrefectureData? {
-    guard let prefectureData = self.prefectureData else { return nil }
+  // 全てのユーザー情報のバリデーションを行うメソッド
+  // MARK: - validateAllFields()
+  func validateAllFields() -> Bool {
+          validateName()
+          validateBirthday()
+          validateBloodType()
+    return nameErrorMessage == nil && birthdayErrorMessage == nil && bloodTypeErrorMessage == nil  // エラーメッセージが全てnilであればtrueを返す
+    }
+  
+  // prefectureDataのlogoUrlの県のイラストをダウンロードする
+  // MARK: - downloadPrefectureImage()
+  func downloadPrefectureImage(from urlString: String) {
+    guard let url = URL(string: urlString) else {  // URLが有効かどうかのチェック
+//      print("URLが無効です")
+      errorMessage = "URLが無効です。"
+      return
+    }
     
-    return prefectureData
+    URLSession.shared.dataTask(with: url) { [weak self] data, response, error in  // [weak self]を使って循環参照を防ぐ
+      if let error = error {
+//        print("画像のダウンロードに失敗しました: ", error.localizedDescription)
+        self?.errorMessage = "画像のダウンロードに失敗しました。"
+        return
+      }
+      if let data = data, let image = UIImage(data: data) {  // 画像のダウンロードに成功した場合
+                      DispatchQueue.main.async {  // メインスレッドで実行
+                          self?.downloadedImage = image
+                      }
+                  }
+    }.resume()  // 非同期で画像をダウンロード
   }
-  
   
 }
--- Regional-Compatibility-Checker/Regional-Compatibility-Checker/Views/ContentView.swift
+++ Regional-Compatibility-Checker/Regional-Compatibility-Checker/Views/ContentView.swift
@@ -13,26 +13,37 @@ struct ContentView: View {
   let birthday = YearMonthDay(year: 2000, month: 1, day: 27)
   let today = YearMonthDay(year: 2023, month: 5, day: 5)
 //  let userData = UserData(name: "ゆめみん", birthday: birthday, bloodType: "AB", today: today)
+  
+  @StateObject var viewModel: UserPrefectureViewModel = UserPrefectureViewModel()
 
     var body: some View {
-      Button(action: {
-        apiService.postUserData(userData: UserData(name: "晴登", birthday: self.birthday, bloodType: "a", today: self.today)
-        ) { result in
-          switch result {
-          case .success(let prefectureData):
-            print(prefectureData)
-            self.result = prefectureData
-            print("通信が成功しました")
-          case .failure(let error):
-            print(error)
-            print("エラーが発生しました")
+      VStack {
+        Button(action: {
+          apiService.postUserData(userData: UserData(name: "晴登", birthday: self.birthday, bloodType: "a", today: self.today)
+          ) { result in
+            switch result {
+            case .success(let prefectureData):
+              print(prefectureData)
+              self.result = prefectureData
+              print("通信が成功しました")
+            case .failure(let error):
+              print(error)
+              print("エラーが発生しました")
+            }
           }
+          print("ボタンが押されました")
+        }) {
+          Text("占い")
         }
-        print("ボタンが押されました")
-      }) {
-        Text("占い")
+        Text(result.name)
+        
+        if let image = viewModel.downloadedImage {
+                    Image(uiImage: image)
+                        // 画像の表示設定
+                } else {
+                    Text("No Image")
+                }
       }
-      Text(result.name)
     }
 }
 
