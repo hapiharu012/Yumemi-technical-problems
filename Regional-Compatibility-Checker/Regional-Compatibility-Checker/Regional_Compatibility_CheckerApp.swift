52e091c3919203bba24745ad9cf89f2591a00a59
haruto <120043995+hapiharu012@users.noreply.github.com>
2024-01-09 22:56:38 +0900

    Revert "Revert "ビューの実装""

---
+++ Regional-Compatibility-Checker/Regional-Compatibility-Checker/Regional_Compatibility_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Regional_Compatibility_CheckerApp.swift
+//  Regional-Compatibility-Checker
+//
+//  Created by k21123kk on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct Regional_Compatibility_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
